82214230233ccf33acc565803f1c31249ab1c9a8
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-19 12:56:48 +0200

    storing events

--- heroesofddd_rails_application/test/real_event_store_integration_test_case.rb
+++ heroesofddd_rails_application/test/real_event_store_integration_test_case.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class RealEventStoreIntegrationTestCase < ActionDispatch::IntegrationTest
+  self.use_transactional_tests = false
 
   def execute_command(command)
     Rails.configuration.command_bus.call(command)
