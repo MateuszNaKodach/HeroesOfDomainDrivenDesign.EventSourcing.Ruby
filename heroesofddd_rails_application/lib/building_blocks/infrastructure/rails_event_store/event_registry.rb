40f0698920a917fe61682e74c96502303c810049
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-21 11:42:57 +0200

    test - pass for mapping rails event to event

--- heroesofddd_rails_application/lib/building_blocks/infrastructure/rails_event_store/event_registry.rb
+++ heroesofddd_rails_application/lib/building_blocks/infrastructure/rails_event_store/event_registry.rb
@@ -5,7 +5,12 @@ module BuildingBlocks
         def initialize
           super
         end
+
         def map_event_type(domain_class, storage_class = nil, to_storage = nil, to_domain = nil) end
+
+        def domain_to_store(domain_event)
+          domain_event
+        end
       end
     end
   end
