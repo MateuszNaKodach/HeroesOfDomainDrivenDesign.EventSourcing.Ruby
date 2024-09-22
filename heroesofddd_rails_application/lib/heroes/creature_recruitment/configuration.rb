5ea167983ac584b32db48736dcb754244e33022f
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-22 14:11:18 +0200

    wip

--- heroesofddd_rails_application/lib/heroes/creature_recruitment/configuration.rb
+++ heroesofddd_rails_application/lib/heroes/creature_recruitment/configuration.rb
@@ -1,10 +1,25 @@
 module Heroes
   module CreatureRecruitment
     class Configuration
-      def call(event_store, command_bus, event_type_mapper)
+      def call(event_store, command_bus, event_registry)
+        event_registry.map_event_type(
+          ::Heroes::CreatureRecruitment::DwellingBuilt,
+          ::EventStore::Heroes::CreatureRecruitment::DwellingBuilt,
+          ->(domain_event) {
+            ::EventStore::Heroes::CreatureRecruitment::DwellingBuilt.from_domain(domain_event)
+          },
+          ->(store_event) {
+            ::EventStore::Heroes::CreatureRecruitment::DwellingBuilt.to_domain(store_event)
+          }
+        )
+        application_service = ::BuildingBlocks::Infrastructure::EventSourcingApplicationService.new(
+          Dwelling,
+          event_store,
+          event_registry
+        ) { |command| "CreatureRecruitment::Dwelling$#{command.dwelling_id}" }
         command_bus.register(
           BuildDwelling,
-          BuildDwellingCommandHandler.new(event_store)
+          BuildDwellingCommandHandler.new(application_service)
         )
       end
     end
