7ddd9a6d18257042a8c30f1ae5669f0793f3f4ed
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-22 21:39:40 +0200

    lib: exclude folders from autoconfiguration - will do better modules etc

--- heroesofddd_rails_application/lib/configuration.rb
+++ heroesofddd_rails_application/lib/configuration.rb
@@ -1,3 +1,5 @@
+require "heroes/configuration"
+
 class Configuration
   def call(event_store, command_bus, query_bus, event_registry)
     Heroes::Configuration.new.call(event_store, command_bus, query_bus, event_registry)
--- heroesofddd_rails_application/lib/heroes/configuration.rb
+++ heroesofddd_rails_application/lib/heroes/configuration.rb
@@ -1,3 +1,5 @@
+require "heroes/creature_recruitment/configuration"
+
 module Heroes
   class Configuration
     def call(event_store, command_bus, query_bus, event_mapper)
