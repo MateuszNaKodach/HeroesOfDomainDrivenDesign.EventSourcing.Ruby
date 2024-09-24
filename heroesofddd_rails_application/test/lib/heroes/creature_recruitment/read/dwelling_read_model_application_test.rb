944ccf9bae3328459af54f3cbef4fe7266178d84
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-24 13:12:31 +0200

    wip: read model

--- heroesofddd_rails_application/test/lib/heroes/creature_recruitment/read/dwelling_read_model_application_test.rb
+++ heroesofddd_rails_application/test/lib/heroes/creature_recruitment/read/dwelling_read_model_application_test.rb
@@ -1,5 +1,6 @@
 require "real_event_store_integration_test_case"
 require "minitest/autorun"
+require "minitest/spec"
 
 module Heroes
   module CreatureRecruitment
@@ -12,8 +13,7 @@ module Heroes
         @stream_name = "CreatureRecruitment::Dwelling$#{@dwelling_id}"
       end
 
-      # todo: parameterize tests
-      def test_projecting_case_1
+      def test_create_on_dwelling_built
         # given
         given_domain_event(@stream_name, DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
 
@@ -24,6 +24,22 @@ module Heroes
         expected_state = DwellingReadModel::State.new(id: @dwelling_id,
                                                       creature_id: @creature_id,
                                                       available_creatures: 0,
+                                                      cost_per_troop: @cost_per_troop)
+        assert_equal expected_state, state
+      end
+
+      def test_update_on_available_creatures_changed
+        # given
+        given_domain_event(@stream_name, DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+        given_domain_event(@stream_name, AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 99))
+
+        # when
+        state = DwellingReadModel::State.find_by(id: @dwelling_id)
+
+        # then
+        expected_state = DwellingReadModel::State.new(id: @dwelling_id,
+                                                      creature_id: @creature_id,
+                                                      available_creatures: 99,
                                                       cost_per_troop: @cost_per_troop)
         assert_equal expected_state, state
       end
