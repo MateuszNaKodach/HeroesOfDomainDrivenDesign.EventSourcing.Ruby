aa62d612fdbe7402fb4776650efccdeef5054bb7
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-26 00:28:24 +0200

    wip: testing

--- heroesofddd_rails_application/test/real_event_store_integration_test_case.rb
+++ heroesofddd_rails_application/test/real_event_store_integration_test_case.rb
@@ -4,6 +4,21 @@ class RealEventStoreIntegrationTestCase < ActionDispatch::IntegrationTest
   include EventStoreTest
   self.use_transactional_tests = false
 
+  def before_setup
+    result = super
+    @previous_command_bus = Rails.configuration.command_bus
+    @recording_command_bus = RecordingCommandBus.new(@previous_command_bus)
+    Rails.configuration.command_bus = @recording_command_bus
+    result
+  end
+
+  def before_teardown
+    result = super
+    @recording_command_bus.reset
+    Rails.configuration.command_bus = @previous_command_bus
+    result
+  end
+
   def execute_command(command)
     command_bus.call(command)
   end
