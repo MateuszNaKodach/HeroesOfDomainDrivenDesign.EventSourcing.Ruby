f3d3ceb15b2384554bc4be70f780991e99d669c5
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-26 01:29:38 +0200

    wip: automation

--- heroesofddd_rails_application/test/lib/heroes/astrologers/automation/when_week_symbol_proclaimed_then_increase_dwelling_available_creatures_automation_test.rb
+++ heroesofddd_rails_application/test/lib/heroes/astrologers/automation/when_week_symbol_proclaimed_then_increase_dwelling_available_creatures_automation_test.rb
@@ -13,7 +13,7 @@ module Heroes
         titan_dwelling_id = given_dwelling_built_event("titan")
 
         # when
-        proclaim_week_symbol = ProclaimWeekSymbol.new(999, 99, "angel", +2)
+        proclaim_week_symbol = ProclaimWeekSymbol.new(2999, 99, "angel", +2)
         execute_command(proclaim_week_symbol)
 
         # then
