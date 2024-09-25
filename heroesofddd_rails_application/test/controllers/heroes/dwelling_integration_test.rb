62139f1bedd7ff232f683a19270a7e237b145a17
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-25 11:12:39 +0200

    wip: tests

--- heroesofddd_rails_application/test/controllers/heroes/dwelling_integration_test.rb
+++ heroesofddd_rails_application/test/controllers/heroes/dwelling_integration_test.rb
@@ -3,23 +3,96 @@ require "test_helper"
 class DwellingsIntegrationTest < ActionDispatch::IntegrationTest
   include EventStoreTest
 
-  test "recruiting creatures" do
-    # Given
-    dwelling_id = SecureRandom.uuid
-    creature_id = "angel"
-    cost_per_troop = Heroes::SharedKernel::Resources::Cost.resources([ :GOLD, 3000 ], [ :GEM, 1 ])
+  def setup
+    @dwelling_id = SecureRandom.uuid
+    @creature_id = "angel"
+    @cost_per_troop = Heroes::SharedKernel::Resources::Cost.resources(
+                                                                        [:GOLD, 3000],
+                                                                        [:GEM, 1]
+                                                                      )
+    @stream_name = "CreatureRecruitment::Dwelling$#{@dwelling_id}"
+  end
 
-    given_domain_event("CreatureRecruitment::Dwelling$#{dwelling_id}",
-                       Heroes::CreatureRecruitment::DwellingBuilt.new(dwelling_id, creature_id, cost_per_troop))
-    given_domain_event("CreatureRecruitment::Dwelling$#{dwelling_id}",
-                       Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(dwelling_id, creature_id, 10))
+  test "viewing the recruitment page" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
 
     # When
-    post recruit_heroes_creature_recruitment_dwelling_path(dwelling_id), params: { recruit_count: 5 }
+    get heroes_creature_recruitment_dwelling_path(@dwelling_id)
 
     # Then
-    assert_redirected_to heroes_creature_recruitment_dwelling_path(dwelling_id)
+    assert_response :success
+    assert_select ".recruitment__title", "Recruit Angels"
+    assert_select ".recruitment__count-value", "10"
+    assert_select "#recruit-count", "0"
+    assert_select ".recruitment__slider-input[max='10']"
+  end
+
+  test "recruiting creatures successfully" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
+
+    # When
+    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 5 }
+
+    # Then
+    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
     follow_redirect!
-    assert_select ".creatures__count .count", "5" # Assuming the view shows the updated count
+    assert_select ".recruitment__count-value", "5"
+  end
+
+  test "attempting to recruit more creatures than available" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 3))
+
+    # When
+    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 5 }
+
+    # Then
+    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
+    follow_redirect!
+    assert_select ".flash-alert", /Failed to recruit creatures/
+  end
+
+  test "recruiting zero creatures" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
+
+    # When
+    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 0 }
+
+    # Then
+    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
+    follow_redirect!
+    assert_select ".recruitment__count-value", "10"
+  end
+
+  test "viewing a non-existent dwelling" do
+    # Given
+    non_existent_id = SecureRandom.uuid
+
+    # When
+    get heroes_creature_recruitment_dwelling_path(non_existent_id)
+
+    # Then
+    assert_response :not_found
+  end
+
+  test "updating total cost when changing recruit count" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
+
+    # When
+    get heroes_creature_recruitment_dwelling_path(@dwelling_id)
+
+    # Then
+    assert_response :success
+    assert_select "script", /document\.getElementById\('total-gold'\)\.textContent = count \* 3000/
+    assert_select "script", /document\.getElementById\('total-gem'\)\.textContent = count \* 1/
   end
 end
