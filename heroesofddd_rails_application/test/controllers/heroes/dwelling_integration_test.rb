b3fa973b0f5512c5d0c46e695ed78e98bf61c575
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-25 11:40:20 +0200

    show flash

--- heroesofddd_rails_application/test/controllers/heroes/dwelling_integration_test.rb
+++ heroesofddd_rails_application/test/controllers/heroes/dwelling_integration_test.rb
@@ -43,34 +43,6 @@ class DwellingsIntegrationTest < ActionDispatch::IntegrationTest
     assert_select ".recruitment__count-value", "5"
   end
 
-  test "attempting to recruit more creatures than available" do
-    # Given
-    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
-    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 3))
-
-    # When
-    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 5 }
-
-    # Then
-    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
-    follow_redirect!
-    assert_select ".flash-alert", /Failed to recruit creatures/
-  end
-
-  test "recruiting zero creatures" do
-    # Given
-    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
-    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
-
-    # When
-    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 0 }
-
-    # Then
-    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
-    follow_redirect!
-    assert_select ".recruitment__count-value", "10"
-  end
-
   test "viewing a non-existent dwelling" do
     # Given
     non_existent_id = SecureRandom.uuid
@@ -94,5 +66,21 @@ class DwellingsIntegrationTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "script", /document\.getElementById\('total-gold'\)\.textContent = count \* 3000/
     assert_select "script", /document\.getElementById\('total-gem'\)\.textContent = count \* 1/
+  end
+
+  test "recruiting zero creatures" do
+    # Given
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::DwellingBuilt.new(@dwelling_id, @creature_id, @cost_per_troop))
+    given_domain_event(@stream_name, Heroes::CreatureRecruitment::AvailableCreaturesChanged.new(@dwelling_id, @creature_id, 10))
+
+    # When
+    post recruit_heroes_creature_recruitment_dwelling_path(@dwelling_id), params: { recruit_count: 0 }
+
+    # Then
+    assert_redirected_to heroes_creature_recruitment_dwelling_path(@dwelling_id)
+    follow_redirect!
+    assert_select ".recruitment__count-value", "10"
+    assert_select ".recruitment__message-box__text", "Please select at least one creature to recruit."
+    assert_select ".recruitment__message-box__text--error"
   end
 end
