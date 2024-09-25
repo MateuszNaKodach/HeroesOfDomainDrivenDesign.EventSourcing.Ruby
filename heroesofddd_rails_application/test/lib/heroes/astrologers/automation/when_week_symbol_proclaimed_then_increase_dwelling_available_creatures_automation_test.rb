25bbc439da7d5ad127cb8faeda7686721c8a76b6
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-26 00:38:17 +0200

    wip: process test

--- heroesofddd_rails_application/test/lib/heroes/astrologers/automation/when_week_symbol_proclaimed_then_increase_dwelling_available_creatures_automation_test.rb
+++ heroesofddd_rails_application/test/lib/heroes/astrologers/automation/when_week_symbol_proclaimed_then_increase_dwelling_available_creatures_automation_test.rb
@@ -1,49 +1,47 @@
+require "real_event_store_integration_test_case"
+require "heroes/astrologers/write/proclaim_week_symbol/command_proclaim_week_symbol"
+
 module Heroes
   module Astrologers
     class WhenWeekSymbolProclaimedThenIncreaseDwellingAvailableCreaturesApplicationTest < RealEventStoreIntegrationTestCase
+      def test_case_1
+        # given
+        angel_dwelling_id_1 = given_dwelling_built_event("angel")
+        angel_dwelling_id_2 = given_dwelling_built_event("angel")
+        titan_dwelling_id = given_dwelling_built_event("titan")
 
-      def test_given_nothing_when_proclaim_week_symbol_then_success
         # when
-        month = 4
-        week = 2
-        week_of = "angel"
-        growth = +5
-        proclaim_week_symbol = ProclaimWeekSymbol.new(month, week, week_of, growth)
+        proclaim_week_symbol = ProclaimWeekSymbol.new(1, 1, "angel", +2)
         execute_command(proclaim_week_symbol)
 
         # then
-        expected_event = WeekSymbolProclaimed.new(month, week, week_of, growth)
-        then_domain_event(@stream_name, expected_event)
+        expected_command_1 = IncreaseAvailableCreatures.new(angel_dwelling_id_1, "angel", +2)
+        then_command_executed(expected_command_1)
+
+        expected_command_2 = IncreaseAvailableCreatures.new(angel_dwelling_id_2, "angel", +2)
+        then_command_executed(expected_command_2)
+
+        not_expected_command = IncreaseAvailableCreatures.new(titan_dwelling_id, "titan", +2)
+        then_command_not_executed(not_expected_command)
       end
 
-      def test_given_week_symbol_proclaimed_when_proclaim_week_symbol_for_the_same_week_then_failed
-        # given
-        month = 1
-        week = 1
-        week_of = "black_dragon"
-        growth = +2
-        given_domain_event(@stream_name, WeekSymbolProclaimed.new(month, week, week_of, growth))
+      private
 
-        # when - then
-        proclaim_week_symbol = ProclaimWeekSymbol.new(month, week, week_of, growth)
-        assert_raises(OnlyOneSymbolPerWeek) do
-          execute_command(proclaim_week_symbol)
-        end
+      def given_dwelling_built_event(creature_id)
+        dwelling_id = SecureRandom.uuid
+        cost_per_troop = Heroes::SharedKernel::Resources::Cost.resources([ :GOLD, 3000 ], [ :GEM, 1 ])
+        stream_name = "CreatureRecruitment::Dwelling$#{@dwelling_id}"
+
+        given_domain_event(stream_name, DwellingBuilt.new(dwelling_id, creature_id, cost_per_troop))
+        dwelling_id
       end
 
-      def test_given_week_symbol_proclaimed_when_proclaim_week_symbol_for_the_past_week_then_failed
-        # given
-        month = 1
-        week = 4
-        week_of = "titan"
-        growth = +3
-        given_domain_event(@stream_name, WeekSymbolProclaimed.new(month, week, week_of, growth))
+      def then_command_executed(command)
+        assert_includes(@recording_command_bus.recorded, command)
+      end
 
-        # when - then
-        proclaim_week_symbol = ProclaimWeekSymbol.new(month, week - 1, week_of, growth)
-        assert_raises(OnlyOneSymbolPerWeek) do
-          execute_command(proclaim_week_symbol)
-        end
+      def then_command_not_executed(command)
+        assert_not_includes(@recording_command_bus.recorded, command)
       end
     end
   end
