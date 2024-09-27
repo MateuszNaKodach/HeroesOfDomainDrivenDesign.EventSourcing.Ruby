0fa1e56bd65044b5342e343b181003ca9e7d86af
Mateusz Nowak <mateusz@nakodach.pl>
2024-09-27 15:32:12 +0200

    render_dwelling works

--- heroesofddd_rails_application/app/helpers/heroes/creature_recruitment/dwellings_helper.rb
+++ heroesofddd_rails_application/app/helpers/heroes/creature_recruitment/dwellings_helper.rb
@@ -1,10 +1,9 @@
 module Heroes
   module CreatureRecruitment
     module DwellingsHelper
-      def render_dwelling(dwelling, form)
+      def render_dwelling(dwelling)
         render partial: "heroes/creature_recruitment/dwellings/dwelling", locals: {
-          dwelling: dwelling,
-          form: form
+          dwelling: dwelling
         }
       end
     end
